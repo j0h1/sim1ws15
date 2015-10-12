37d30bb229944b41534de452b9a8ef709e6f56dc
Felix Schuller <e1025256@student.tuwien.ac.at>
2015-10-12 17:03:47 +0200

    Updated path in test.m

--- test.m
+++ test.m
@@ -2,7 +2,7 @@ function [ output_args ] = test( input_args )
 %TEST Summary of this function goes here
 %   Detailed explanation goes here
 
-filename = 'sim12-evaluation-dataset-wt15-part-01\1_2015-10-03_13-42-32.mp4';
+filename = 'data\1_2015-10-03_13-42-32.mp4';
 
 % video
 videoReader = VideoReader(filename);
